2ad5c492dcd35936d18525b0ca0ade31295282a7
fwtn <fwtn+git@fireturtle.net>
2020-04-22 10:56:48 +0900

    Update lib.rs

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,5 +1,8 @@
 extern crate chrono;
-
+use chrono::{TimeZone, Weekday,ParseResult};
+use chrono::prelude::{DateTime, Utc, Local, Datelike, Timelike};
+mod alarm;
+mod timer;
 
 #[cfg(test)]
 mod tests {
@@ -7,17 +10,34 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+    fn test_now(){
+        
+    }
 }
-pub struct TimeKeeper{
-    time: i32,
-
+pub struct Timekeeper{
+    lasttime: i32,
+    currenttime: DateTime<Local>
 }
 impl Timekeeper{
-    pub fn getHour(){}
-    pub fn getWeek(){}
-    pub fn getMinute(){}
-    pub fn Update(){}
-    fn SetCurrentTime(){}
+    pub fn new() -> Timekeeper{
+        Timekeeper{currenttime: Utc::now()}
+    }
+    pub fn getHour(&self) -> u32{
+        self.currenttime.year()      
+    }
+    pub fn getWeek(&self) -> u32{
+        self.currenttime.weekday()
+    }
+    pub fn getMinute(&self) -> u32{
+        self.currenttime.minute()
+    }
+    pub fn getSecond(&self) -> u32{
+        self.currenttime.second()
+    }
+    pub fn update(){}
+    fn setTime(&self){
+        self.currenttime = Utc::now()
+    }
     fn CheckAlerm(){}
     fn CheckTimer(){}
 }
